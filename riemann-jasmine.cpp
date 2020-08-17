1664e0a56ba863e58f38487b91c52bcc3deef1d0
Jasmine Hakim-Elahi <jasmine.hakim@berkeley.edu>
2020-08-17 14:00:57 -0700

    Add Jasmine's homework file for day 6.  Computing pi using integration via summation (Riemann sum)

---
+++ riemann-jasmine.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+double compute_integrand(double x) {
+    double integrand = 1.0 / (1.0 + x * x);
+
+    return integrand;
+
+}
+
+double compute_integral(double a, double b, int npoints) {
+    double dx = (b - a) / npoints;
+    
+    double position = a + dx / 2.0;
+    double sum = 0.0;
+    for (int i = 0; i < npoints; i++) {
+        sum += dx * compute_integrand(position + i * dx);
+    }
+
+    return sum;
+}
+
+int main(void) {
+
+    std:: cout << 4.0 * compute_integral(0.0, 1.0, 190) << std:: endl;
+    return 0;
+}
